9313d455fd66088e9652e94139bb6d9a34f50905
李运辰2021 <17801157815@163.com>
2021-03-11 22:23:22 +0800

    2021-3-11李运辰

---
+++ analysis.py
@@ -0,0 +1,193 @@
+# -*- coding: utf-8 -*-
+#encoding=utf-8
+
+"""
+李运辰 2021-3-11
+
+公众号：python爬虫数据分析挖掘
+"""
+
+# 导包
+import pandas as pd
+import os
+# from pyecharts import options as opts
+# from pyecharts.globals import ThemeType
+# from pyecharts.charts import Bar
+# from pyecharts.charts import Pie
+from stylecloud import gen_stylecloud
+import jieba
+
+#读入数据
+df_all = pd.read_csv("笔记本电脑-李运辰.csv",engine="python")
+df = df_all.copy()
+# 重置索引
+df = df.reset_index(drop=True)
+
+#展示每个品牌的数据量
+def an1():
+    brand_counts = df.groupby('brand')['price'].count().sort_values(ascending=False).reset_index()
+    brand_counts.columns = ['品牌', '数据量']
+    name = (brand_counts['品牌']).tolist()
+    dict_values = (brand_counts['数据量']).tolist()
+
+    ##去掉英文名称
+    for i in range(0,len(name)):
+        if "（" in name[i]:
+            name[i] = name[i][0:int(name[i].index("（"))]
+
+    #链式调用
+    c = (
+        Bar(
+            init_opts=opts.InitOpts(  # 初始配置项
+                theme=ThemeType.MACARONS,
+                animation_opts=opts.AnimationOpts(
+                    animation_delay=1000, animation_easing="cubicOut"  # 初始动画延迟和缓动效果
+                ))
+        )
+            .add_xaxis(xaxis_data=name)  # x轴
+            .add_yaxis(series_name="展示每个品牌的数据量", yaxis_data=dict_values)  # y轴
+            .set_global_opts(
+            title_opts=opts.TitleOpts(title='', subtitle='',  # 标题配置和调整位置
+                                      title_textstyle_opts=opts.TextStyleOpts(
+                                          font_family='SimHei', font_size=25, font_weight='bold', color='red',
+                                      ), pos_left="90%", pos_top="10",
+                                      ),
+            xaxis_opts=opts.AxisOpts(name='品牌', axislabel_opts=opts.LabelOpts(rotate=45)),
+            # 设置x名称和Label rotate解决标签名字过长使用
+            yaxis_opts=opts.AxisOpts(name='数据量'),
+
+        )
+            .render("展示每个品牌的数据量.html")
+    )
+
+#最高价格对比
+def an2():
+    brand_maxprice = df.groupby('brand')['price'].agg(['max'])['max'].sort_values(ascending=False).reset_index()
+    brand_maxprice.columns = ['品牌', '最高价']
+    name = (brand_maxprice['品牌']).tolist()
+    dict_values = (brand_maxprice['最高价']).tolist()
+
+    ##去掉英文名称
+    for i in range(0, len(name)):
+        if "（" in name[i]:
+            name[i] = name[i][0:int(name[i].index("（"))]
+
+    # 链式调用
+    c = (
+        Bar(
+            init_opts=opts.InitOpts(  # 初始配置项
+                theme=ThemeType.MACARONS,
+                animation_opts=opts.AnimationOpts(
+                    animation_delay=1000, animation_easing="cubicOut"  # 初始动画延迟和缓动效果
+                ))
+        )
+            .add_xaxis(xaxis_data=name)  # x轴
+            .add_yaxis(series_name="最高价格对比", yaxis_data=dict_values)  # y轴
+            .set_global_opts(
+            title_opts=opts.TitleOpts(title='', subtitle='',  # 标题配置和调整位置
+                                      title_textstyle_opts=opts.TextStyleOpts(
+                                          font_family='SimHei', font_size=25, font_weight='bold', color='red',
+                                      ), pos_left="90%", pos_top="10",
+                                      ),
+            xaxis_opts=opts.AxisOpts(name='品牌', axislabel_opts=opts.LabelOpts(rotate=45)),
+            # 设置x名称和Label rotate解决标签名字过长使用
+            yaxis_opts=opts.AxisOpts(name='最高价'),
+
+        )
+            .render("最高价格对比.html")
+    )
+
+#价格均值
+def an3():
+    brand_meanprice = df.groupby('brand')['price'].agg(['mean'])['mean'].sort_values(ascending=False).reset_index()
+    brand_meanprice.columns = ['品牌', '价格均值']
+    name = (brand_meanprice['品牌']).tolist()
+    dict_values = (brand_meanprice['价格均值']).tolist()
+
+    ##去掉英文名称
+    for i in range(0, len(name)):
+        if "（" in name[i]:
+            name[i] = name[i][0:int(name[i].index("（"))]
+
+    #价格转为整数
+    for i in range(0, len(dict_values)):
+            dict_values[i] = int(dict_values[i])
+
+
+    # 链式调用
+    c = (
+        Bar(
+            init_opts=opts.InitOpts(  # 初始配置项
+                theme=ThemeType.MACARONS,
+                animation_opts=opts.AnimationOpts(
+                    animation_delay=1000, animation_easing="cubicOut"  # 初始动画延迟和缓动效果
+                ))
+        )
+            .add_xaxis(xaxis_data=name)  # x轴
+            .add_yaxis(series_name="价格均值对比", yaxis_data=dict_values)  # y轴
+            .set_global_opts(
+            title_opts=opts.TitleOpts(title='', subtitle='',  # 标题配置和调整位置
+                                      title_textstyle_opts=opts.TextStyleOpts(
+                                          font_family='SimHei', font_size=25, font_weight='bold', color='red',
+                                      ), pos_left="90%", pos_top="10",
+                                      ),
+            xaxis_opts=opts.AxisOpts(name='品牌', axislabel_opts=opts.LabelOpts(rotate=45)),
+            # 设置x名称和Label rotate解决标签名字过长使用
+            yaxis_opts=opts.AxisOpts(name='价格均值'),
+
+        )
+            .render("价格均值对比.html")
+    )
+
+#各大品牌标题词云
+def an4():
+    brand_title = df.groupby('brand')['title']
+    brand_title = list(brand_title)
+
+
+    for z in range(0,len(brand_title)):
+        brandname = brand_title[z][0]
+        if "（" in brandname:
+            brandname = brandname[0:int(brandname.index("（"))]
+        brandname = str(brandname).encode("utf-8").decode('utf8')
+        print(brandname)
+        text = "".join((brand_title[z][1]).tolist())
+        text = text.replace(brand_title[z][0],"").replace(brandname,"").replace("\n\r","").replace("\t","").replace("\n","").replace("\r","").replace("【","").replace("】","").replace(" ","")
+        #print(text)
+        with open("text/"+str(brandname)+".txt","a+") as f:
+            f.write(text)
+
+def an4_pic():
+        ###词云图标
+        fa_list = ['fas fa-play', 'fas fa-audio-description', 'fas fa-circle', 'fas fa-eject', 'fas fa-stop',
+                   'fas fa-video', 'fas fa-volume-off', 'fas fa-truck', 'fas fa-apple-alt', 'fas fa-mountain',
+                   'fas fa-tree', 'fas fa-database', 'fas fa-wifi', 'fas fa-mobile', 'fas fa-plug']
+        z=0
+        ##开始绘图
+        for filename in os.listdir("text"):
+            print(filename)
+            with open("text/"+filename,"r") as f:
+                 text = (f.readlines())[0]
+
+            with open("stopword.txt", "r", encoding='UTF-8') as f:
+                stopword = f.readlines()
+            for i in stopword:
+                print(i)
+                i = str(i).replace("\r\n", "").replace("\r", "").replace("\n", "")
+                text = text.replace(i, "")
+            word_list = jieba.cut(text)
+            result = " ".join(word_list)  # 分词用 隔开
+            # 制作中文云词
+            icon_name = str(fa_list[z])
+            gen_stylecloud(text=result, icon_name=icon_name, font_path='simsun.ttc',output_name=str(filename.replace(".txt",""))+"词云图.png")  # 必须加中文字体，否则格式错误
+            z =z+1
+
+#展示每个品牌的数据量
+#an1()
+#最高价格对比
+#an2()
+#价格均值
+#an3()
+#各大品牌标题词云
+#an4()
+an4_pic()
---
+++ scrapy_data.py
@@ -0,0 +1,160 @@
+# -*- coding: utf-8 -*-
+
+
+"""
+李运辰 2021-3-11
+
+公众号：python爬虫数据分析挖掘
+"""
+
+
+import requests
+from lxml import etree
+import json
+import time
+headers = {
+            'user-agent':'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/69.0.3947.100 Safari/537.36',
+        }
+
+
+import openpyxl
+outwb = openpyxl.Workbook()
+outws = outwb.create_sheet(index=0)
+outws.cell(row=1, column=1, value="index")
+outws.cell(row=1, column=2, value="brand")
+outws.cell(row=1, column=3, value="title")
+outws.cell(row=1, column=4, value="price")
+count = 2
+###根据商品id获取评论数
+def commentcount(product_id):
+    url = "https://club.jd.com/comment/productCommentSummaries.action?referenceIds="+str(product_id)+"&callback=jQuery8827474&_=1615298058081"
+    res = requests.get(url, headers=headers)
+    res.encoding = 'gbk'
+    text = (res.text).replace("jQuery8827474(","").replace(");","")
+    text = json.loads(text)
+    comment_count = text['CommentsCount'][0]['CommentCountStr']
+
+    comment_count = comment_count.replace("+", "")
+    ###对“万”进行操作
+    if "万" in comment_count:
+        comment_count = comment_count.replace("万","")
+        comment_count = str(int(comment_count)*10000)
+    return comment_count
+
+
+#commentcount("71929438514")
+###获取每一页的商品数据
+def getlist(url,brand):
+    global  count
+    #url="https://search.jd.com/search?keyword=笔记本&wq=笔记本&ev=exbrand_联想%5E&page=9&s=241&click=1"
+    res = requests.get(url,headers=headers)
+    res.encoding = 'utf-8'
+    text = res.text
+
+
+    selector = etree.HTML(text)
+    list = selector.xpath('//*[@id="J_goodsList"]/ul/li')
+
+    for i in list:
+        title=i.xpath('.//div[@class="p-name p-name-type-2"]/a/em/text()')[0]
+        price = i.xpath('.//div[@class="p-price"]/strong/i/text()')[0]
+        #product_id = i.xpath('.//div[@class="p-commit"]/strong/a/@id')[0].replace("J_comment_","")
+
+
+        ###获取商品评论数
+        #comment_count = commentcount(product_id)
+        #print("title="+str(title))
+        #print("price="+str(price))
+        #print("comment_count="+str(comment_count))
+
+        outws.cell(row=count, column=1, value=str(count-1))
+        outws.cell(row=count, column=2, value=str(brand))
+        outws.cell(row=count, column=3, value=str(title))
+        outws.cell(row=count, column=4, value=str(price))
+
+
+        count = count +1
+        #print("目前条数="+str(count))
+        #print("-----")
+
+
+#遍历每一页
+def getpage(brand_dict):
+    global  count
+    for k, v in brand_dict.items():
+
+        page = 1
+        s = 1
+        brand = str(k)
+        try:
+            for i in range(1, int(v) + 1):
+                url = "https://search.jd.com/search?keyword=笔记本&wq=笔记本&ev=exbrand_" + str(brand) + "&page=" + str(
+                    page) + "&s=" + str(s) + "&click=1"
+                getlist(url, brand)
+                page = page + 2
+                s = s + 60
+                # if int(i) %3==0:
+                #     time.sleep(2)
+                print("品牌=" + str(k) + ",页数=" + str(v) + ",当前页数=" + str(i))
+        except:
+            pass
+
+
+
+    outwb.save("笔记本电脑-李运辰.xls")  # 保存
+
+
+
+#开始爬取
+brand_dict={
+    '联想（lenovo）':100,
+    'ThinkPad':100,
+    '戴尔（DELL）':100,
+    '惠普（HP）':100,
+    '华为（HUAWEI）':100,
+    'Apple':100,
+    '小米（MI）':47,
+    '宏碁（acer）':43,
+    '荣耀（HONOR）':21,
+    '机械革命（MECHREVO）':31,
+    '微软（Microsoft）':100,
+    'LG':3,
+    '神舟（HASEE）':34,
+    'VAIO':3,
+    '三星（SAMSUNG）':47,
+}
+
+getpage(brand_dict)
+
+
+"""
+联想
+ThinkPad
+戴尔
+惠普
+华为
+Apple
+小米
+宏碁
+荣耀
+机械革命
+微软
+LG
+神州
+VAIO
+三星
+
+
+1.入门爬虫（京东商品数据为例）
+2.如何获取网页标签内容
+3.获取评论数
+4.如何通过python将数据保存到excel
+5.遍历下一页
+
+
+源码获取方式：
+公众号：python爬虫数据分析挖掘
+
+回复：京东商品
+
+"""
